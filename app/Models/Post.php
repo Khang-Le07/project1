d0e9020adff742f4553206d85d36d1915993fffa
Khang-Le07 <letrannguyenkhang2001@gmail.com>
2023-06-07 14:08:39 +0700

    Update Post.php

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = 
+    [
+        'id',
+        'title',
+        'body',
+        'img_path',
+        'is_published',
+    ];
+    protected $table = "posts";
 }
